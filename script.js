ea4678f30bc0b93ca877675eccbae891d4e248ab
mrspok407 <mr.spok407@gmail.com>
2020-08-13 12:34:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,17 @@ const randomSeasonAndEpisode = ({ seasonRange = [5, 10], episodeRange = [1, 23] 
 	const episodeNum = Math.ceil(
 		Math.random() * (episodeRange[1] - episodeRange[0]) + episodeRange[0]
 	);
+	
+	const episodeModified = seasonNum === 10 && episodeNum > 17 ? Math.ceil(Math.random() * 17) : episodeNum
 
 	seasonNumber.innerHTML = seasonNum;
-	episodeNumber.innerHTML = episodeNum;
-
+	episodeNumber.innerHTML = episodeModified
+		
 	setTimeout(() => {
 		const link =
 			seasonNum === 10
-				? `https://friends-online.me/series/${seasonNum}s${Math.ceil(Math.random() * 17)}e/`
-				: `https://friends-online.me/series/s${seasonNum}e${episodeNum}/`;
+				? `https://friends-online.me/series/${seasonNum}s${episodeModified}e/`
+				: `https://friends-online.me/series/s${seasonNum}e${episodeModified}/`;
 
 		window.location.href = link;
 	}, 1000);

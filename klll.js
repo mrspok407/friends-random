d06eb0c16ab22d6b46cd95c6da316c2dccef8bab
mrspok407 <mr.spok407@gmail.com>
2020-08-20 17:14:08 +0300

    Update and rename kkk.js to klll.js

--- klll.js
+++ klll.js
@@ -18,8 +18,8 @@
       left: 0;
       top: 0;
       width: 100%;
-      height: 100%;
-      overflow: auto;
+      height: 100vh;
+      overflow: hidden;
       background-color: rgb(0,0,0);
       background-color: rgba(0,0,0,0.4);
     }

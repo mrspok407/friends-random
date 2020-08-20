76a84d52e601a27ace7fc53b6ef637271c387887
mrspok407 <mr.spok407@gmail.com>
2020-08-20 17:11:13 +0300

    Update and rename uqqqqu.js to jjjjj.js

--- jjjjj.js
+++ jjjjj.js
@@ -12,18 +12,17 @@
     }
 
     .model {
-      
-      position: fixed; 
-      z-index: 9999; 
+
+      position: fixed;
+      z-index: 9999;
       left: 0;
       top: 0;
-      width: 100%; 
-      height: 100%; 
-      overflow: auto; 
+      width: 100%;
+      height: 100%;
+      overflow: auto;
       background-color: rgb(0,0,0);
       background-color: rgba(0,0,0,0.4);
     }
-
 
     `
       document.head.appendChild(style)
@@ -43,7 +42,8 @@
         const modal = document.createElement("div")
         modal.className = "modal"
         modal.innerHTML = "Hello World"
+
+        document.body.appendChild(modal)
       })
     })
-
 

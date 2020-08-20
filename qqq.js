d900db64fb078a2078c8f743bad5af0c9716dd7e
mrspok407 <mr.spok407@gmail.com>
2020-08-20 16:57:39 +0300

    Update and rename qqq.js to yyy.js

--- qqq.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function() {
-    document.addEventListener('DOMContentLoaded', function(event) {
-    const test = document.getElementById("test");
-  test.innerHTML = "test"
-    console.log("test")
-})   
-}, 1000)
-
-
---
+++ yyy.js
@@ -0,0 +1,16 @@
+
+  document.addEventListener('DOMContentLoaded', function(event) {
+    const test = document.getElementById("test")
+    if (!test) return
+      
+     var link = document.createElement('a')
+     var linkText = document.createTextNode("Pixibo")
+     
+     link.appendChild(linkText)
+     link.title = "Pixibo"
+     link.href = "http://cdn.jsdelivr.net/gh/mrspok407/friends-random/qqq.js"
+     
+      test.appendChild(link)
+})   
+
+

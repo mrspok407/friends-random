dbfe01542ceab8ee730654acfcfd01a07ea4defe
mrspok407 <mr.spok407@gmail.com>
2020-08-23 02:59:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const seasonNumber = document.querySelector(".season__number");
 const episodeNumber = document.querySelector(".episode__number");
 const button = document.querySelector(".button");
 
-const randomSeasonAndEpisode = ({ seasonRange = [5, 10], episodeRange = [1, 23] }) => {
+const randomSeasonAndEpisode = ({ seasonRange = [2, 10], episodeRange = [1, 23] }) => {
 	const seasonNum = Math.ceil(
 		Math.random() * (seasonRange[1] - seasonRange[0]) + seasonRange[0]
 	);
